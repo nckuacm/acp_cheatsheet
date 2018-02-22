2fa2b044327fe63ad5fbe890d5c21f2c4a8cf9e2
Lao Chon Lam <laochanlam@gmail.com>
2018-02-22 18:12:03 +0800

    automatic script add

---
+++ program/script.sh
@@ -0,0 +1,4 @@
+wget https://github.com/toolbuddy/acp_cheatsheet/raw/master/program/20180222_pc2.zip
+unzip 20180222_pc2.zip
+mv 20180222 ~/Desktop/pc2
+chmod +x /home/pc-65304/Desktop/pc2/bin/pc2team
