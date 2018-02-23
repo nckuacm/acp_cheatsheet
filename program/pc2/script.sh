42bdfe9a6ac6f64b4557f2285bb030f526303b74
laochanlam <laochanlam@gmail.com>
2018-02-23 14:13:16 +0800

    script modify

--- program/pc2/script.sh
+++ program/pc2/script.sh
@@ -1,4 +1,17 @@
-wget https://github.com/toolbuddy/acp_cheatsheet/raw/master/program/20180222_pc2.zip
-unzip 20180222_pc2.zip
-mv 20180222 ~/Desktop/pc2
-chmod +x /home/pc-65304/Desktop/pc2/bin/pc2team
+date="20180223"
+
+wget https://github.com/toolbuddy/acp_cheatsheet/raw/master/program/pc2/20180223/pc2_$date.zip
+wget https://github.com/toolbuddy/acp_cheatsheet/raw/master/program/pc2/20180223/questions_$date.zip
+unzip pc2_$date.zip
+mv pc2_$date /home/pc-65304/Desktop/pc2_$date
+mv questions_$date /home/pc-65304/questions_$date
+mv 
+chmod +x /home/pc-65304/Desktop/pc2_$date/bin/pc2team
+clear
+echo "/*************************************/"
+echo "/                                     /"
+echo "/             Success!                /"
+echo "/                                     /"
+echo "/      Now check your Desktop!        /"
+echo "/                                     /"
+echo "/*************************************/"
