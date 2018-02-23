5a80c47cb508a41898d5bfbbdbc124dfa2d66652
laochanlam <laochanlam@gmail.com>
2018-02-23 14:17:47 +0800

    specify url

--- program/pc2/script.sh
+++ program/pc2/script.sh
@@ -1,7 +1,7 @@
 date="20180223"
 
-wget https://github.com/toolbuddy/acp_cheatsheet/raw/master/program/pc2/20180223/pc2_$date.zip
-wget https://github.com/toolbuddy/acp_cheatsheet/raw/master/program/pc2/20180223/questions_$date.zip
+wget https://github.com/toolbuddy/acp_cheatsheet/raw/master/program/pc2/$date/pc2_$date.zip
+wget https://github.com/toolbuddy/acp_cheatsheet/raw/master/program/pc2/$date/questions_$date.zip
 unzip pc2_$date.zip
 mv pc2_$date /home/pc-65304/Desktop/pc2_$date
 mv questions_$date /home/pc-65304/questions_$date
