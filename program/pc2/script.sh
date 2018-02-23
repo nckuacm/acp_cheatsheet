2424e55e2672746fc2b248a9e13ef01a5ee442c4
laochanlam <laochanlam@gmail.com>
2018-02-23 17:33:54 +0800

    script bug fixed

--- program/pc2/script.sh
+++ program/pc2/script.sh
@@ -1,4 +1,4 @@
-date="20180222"
+date="20180223"
 
 wget https://github.com/toolbuddy/acp_cheatsheet/raw/master/program/pc2/$date/pc2_$date.zip
 wget https://github.com/toolbuddy/acp_cheatsheet/raw/master/program/pc2/$date/questions_$date.zip
@@ -6,7 +6,7 @@ unzip pc2_$date.zip -d pc2_$date
 rm pc2_$date.zip
 mv pc2_$date /home/$USER/Desktop/pc2_$date
 
-mv questions_$date.zip /home/$USER/questions_$date
+mv questions_$date.zip /home/$USER/Desktop/questions_$date.zip
 
 chmod +x /home/$USER/Desktop/pc2_$date/bin/pc2team
 
