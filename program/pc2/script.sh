5d68cc37461382783520f2b8b6514ae4492ce937
laochanlam <laochanlam@gmail.com>
2018-02-23 14:31:11 +0800

    script completed

--- program/pc2/script.sh
+++ program/pc2/script.sh
@@ -1,13 +1,15 @@
-date="20180223"
+date="20180222"
 
 wget https://github.com/toolbuddy/acp_cheatsheet/raw/master/program/pc2/$date/pc2_$date.zip
 wget https://github.com/toolbuddy/acp_cheatsheet/raw/master/program/pc2/$date/questions_$date.zip
-unzip pc2_$date.zip
-mv pc2_$date /home/pc-65304/Desktop/pc2_$date
-mv questions_$date /home/pc-65304/questions_$date
-mv 
-chmod +x /home/pc-65304/Desktop/pc2_$date/bin/pc2team
-clear
+unzip pc2_$date.zip -d pc2_$date
+rm pc2_$date.zip
+mv pc2_$date /home/$USER/Desktop/pc2_$date
+
+mv questions_$date.zip /home/$USER/questions_$date
+
+chmod +x /home/$USER/Desktop/pc2_$date/bin/pc2team
+
 echo "/*************************************/"
 echo "/                                     /"
 echo "/             Success!                /"
